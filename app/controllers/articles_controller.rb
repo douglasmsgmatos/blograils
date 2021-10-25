241452a1025cc77643e8942e3594c92fd68be469
Douglas Martins <douglas.matosje@gmail.com>
2021-10-25 00:39:08 -0300

    Add action update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,15 +21,30 @@ class ArticlesController < ApplicationController
   
     @article = Article.new(article_params)
 
-    if @article.save
-      
-      redirect_to @article
-    
-    else
-      
+    if @article.save      
+      redirect_to @article    
+    else     
       render :new
-    
     end
+
+  end
+
+  def edit
+  
+    @article = Article.find(params[:id])
+  
+  end
+
+  def update
+  
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  
   end
 
   private
@@ -39,6 +54,4 @@ class ArticlesController < ApplicationController
       params.require(:article).permit(:title, :body)
     
     end
-
-  end
 end
