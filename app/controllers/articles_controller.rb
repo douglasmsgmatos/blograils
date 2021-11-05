9646e29a70cc51ea2157b1d8e5a11f272df60fd0
Douglas Martins <douglas.matosje@gmail.com>
2021-11-04 22:25:08 -0300

    Add new method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,14 @@ class ArticlesController < ApplicationController
   
   def index
 
+    @highlights = Article.order(created_at: :desc).first(3)
+
     current_page = (params[:page] || 1).to_i
+    highlight_ids = @highlights.pluck(:id).join(',') 
+
     @articles = Article.order(created_at: :desc).page(current_page).per(2)
+                       .where("id NOT IN(#{highlight_ids})")
+                       .page(current_page).per(2)
 
   end
 
