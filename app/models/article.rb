ba1346a367c081cc30c1da4487b9d55a4436af31
Douglas Martins <douglas.matosje@gmail.com>
2021-11-16 15:59:01 -0300

    Add new refatoring

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,9 @@ class Article < ApplicationRecord
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10 }
 
+    paginates_per 2
+    
+    scope :desc_order, -> { order(created_at: :desc) }
+    scope :without_highlights, ->(ids) { where("id NOT IN(#{ids})") }
+
 end
