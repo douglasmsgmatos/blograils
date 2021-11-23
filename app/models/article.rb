132006fb1fca330f72bc4cb0ff631ffb3eabe4e3
Douglas Martins <douglas.matosje@gmail.com>
2021-11-22 22:42:20 -0300

    Added categories features

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
     include Visible
     has_many  :comments
+    belongs_to :category 
     
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_25_055058) do
+ActiveRecord::Schema.define(version: 2021_11_18_163927) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2021_10_25_055058) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "status"
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_articles_on_category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "comments", force: :cascade do |t|
@@ -30,5 +38,6 @@ ActiveRecord::Schema.define(version: 2021_10_25_055058) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  add_foreign_key "articles", "categories"
   add_foreign_key "comments", "articles"
 end
